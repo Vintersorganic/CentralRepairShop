2bebc18a7557702d9c8e285837aa4e7892bcd8b3
Jorge Daniel Gorostegui Di Peto <jorge.gdp@gmail.com>
2021-01-16 14:49:23 -0300

    Contact Form & Fixed Navbar

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ $(document).ready(function() {
       $(window).scroll(function() {    
           var scroll = $(window).scrollTop();
       
-           //>=, not <=
+           
           if (scroll >= 60) {
-              //clearHeader, not clearheader - caps H
-              $(".navbar").addClass("bg-light");
+              
+              $(".navbar").addClass("bg-dark");
           } else {
-            $(".navbar").removeClass("bg-light");
+            $(".navbar").removeClass("bg-dark");
           }
-      }); //missing );
-        
-      // document ready  
+      });
       });
